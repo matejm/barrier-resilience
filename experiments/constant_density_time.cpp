8df30509fd71306c8a2b554e3fd2755b4563bbec
Matej Marinko <matej@zerodays.dev>
2023-09-03 19:22:24 +0200

    feat: average time results

--- experiments/constant_density_time.cpp
+++ experiments/constant_density_time.cpp
@@ -7,14 +7,28 @@ int main() {
     const auto config_trivial = Config<int>::with_trivial_datastructure();
     const auto config_kdtree = Config<int>::with_kdtree();
 
-    for (int i = 0; i < 100000; i += 100) {
-        auto params = ProblemParams{10, 0, 100, 0, i / 10, i};
+    // Evaluate 5-times and take the average
+    const auto repeats = 5;
 
-        auto times = compare_algorithms(params, {config_trivial, config_kdtree},
-                                        {Algorithm::FordFulkerson}
-        );
+    for (int i = 0; i < 5001; i += 100) {
 
-        std::cout << i << "," << times[0] << "," << times[1] << "," << times[2] << std::endl;
+        double times_trivial = 0, times_kdtree = 0, times_ff = 0;
+
+        for (int j = 0; j < repeats; j++) {
+
+            auto params = ProblemParams{10, 0, 100, 0, i / 20, i};
+
+            auto times = compare_algorithms(params, {config_trivial, config_kdtree},
+                                            {Algorithm::FordFulkerson}
+            );
+
+            times_trivial += times[0];
+            times_kdtree += times[1];
+            times_ff += times[2];
+        }
+
+        std::cout << i << "," << times_trivial / repeats << "," << times_kdtree / repeats << "," << times_ff / repeats
+                  << std::endl;
     }
 
     return 0;
